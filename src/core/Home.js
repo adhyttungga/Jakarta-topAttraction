8dc93a3205c70c8c55329c82a9d0abc89e272d6b
cap7nemo <adhytiatungga09031996@gmail.com>
2021-12-31 17:35:27 +0700

    modified data

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,74 +1,80 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Header from './components/Header'
-import Menu from './components/Menu'
-import Content from './components/Content'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Content from "./components/Content";
 
 // load local json file with import method
-// import data from './data.json' 
+// import data from './data.json'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-  }
-}))
+  },
+}));
 
 const Home = () => {
-  const classes = useStyles()
-  const [state, setState] = React.useState(undefined)
-  const [zoom, setZoom] = React.useState(15)
+  const classes = useStyles();
+  const [state, setState] = React.useState(undefined);
+  const [zoom, setZoom] = React.useState(15);
   const [center, setCenter] = React.useState({
     lat: -6.17441,
     lng: 106.82728,
-  })
-  const [data, setData] = React.useState([])
+  });
+  const [data, setData] = React.useState([]);
 
   const toggleCancel = () => {
-    setState(undefined)
-    setZoom(15)
-    setCenter({...center, lat: -6.17441, lng: 106.82728,})
-  }
-  
+    setState(undefined);
+    setZoom(15);
+    setCenter({ ...center, lat: -6.17441, lng: 106.82728 });
+  };
+
   const toggleMarker = (newState, lat, lng) => {
-    setState(newState)
-    setZoom(17)
-    setCenter({...center, lat: lat, lng: lng})
-  }
-  
+    setState(newState);
+    setZoom(17);
+    setCenter({ ...center, lat: lat, lng: lng });
+  };
+
   // load local json file with fetch method
   const getData = () => {
-    fetch('./jakarta.json', {
+    fetch("./data.json", {
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
-      }
+        Accept: "application/json",
+      },
     })
       .then((res) => {
-        console.log(res)
-        return res.json()
+        console.log(res);
+        return res.json();
       })
       .then((myJson) => {
-        console.log(myJson)
-        setData(myJson)
+        console.log(myJson);
+        setData(myJson);
       })
       .catch((err) => {
-        console.log(err, ' error')
-      })
-  }
+        console.log(err, " error");
+      });
+  };
 
   React.useLayoutEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
     <div className={classes.root}>
-      <Menu state={state} stores={data} toggleMarker={toggleMarker}/>
+      <Menu state={state} stores={data} toggleMarker={toggleMarker} />
       <div>
-        <Header toggleCancel={toggleCancel}/>
-        <Content state={state} zoom={zoom} stores={data} center={center} toggleMarker={toggleMarker}/>
+        <Header toggleCancel={toggleCancel} />
+        <Content
+          state={state}
+          zoom={zoom}
+          stores={data}
+          center={center}
+          toggleMarker={toggleMarker}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
