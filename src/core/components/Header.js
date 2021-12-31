e18163309db3b1b9b2908110e742189752dcbb88
cap7nemo <adhytiatungga09031996@gmail.com>
2021-12-31 15:40:09 +0700

    change singapore to jakarta

--- src/core/components/Header.js
+++ src/core/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import SettingsIcon from '@material-ui/icons/Settings'
-import HelpIcon from '@material-ui/icons/Help'
-import CancelIcon from '@material-ui/icons/Cancel'
-import { makeStyles } from '@material-ui/core/styles'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import SettingsIcon from "@material-ui/icons/Settings";
+import HelpIcon from "@material-ui/icons/Help";
+import CancelIcon from "@material-ui/icons/Cancel";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   appbar: {
     flexGrow: 1,
     justifyContent: "space-around",
@@ -16,30 +16,30 @@ const useStyles = makeStyles(theme => ({
     minWidth: 1420,
     backgroundColor: "#e6e6e6",
     "& > *": {
-      color: "#647d81"
-    }
+      color: "#647d81",
+    },
   },
   headerText: {
     flexGrow: 1,
-    color: "#292b38"
+    color: "#292b38",
   },
   button: {
     "& svg": {
-      fontSize: "1.94rem"
-    }
+      fontSize: "1.94rem",
+    },
   },
-}))
+}));
 
 const Header = (props) => {
-  const classes = useStyles()
-  const { toggleCancel } = props
+  const classes = useStyles();
+  const { toggleCancel } = props;
 
   return (
     <React.Fragment>
       <AppBar position="static" className={classes.appbar}>
         <Toolbar>
           <Typography variant="h6" className={classes.headerText}>
-            <strong>TOP-RATED TOURIST ATTRACTIONS IN SINGAPORE</strong>
+            <strong>TOP-RATED TOURIST ATTRACTIONS IN JAKARTA</strong>
           </Typography>
           <IconButton className={classes.button}>
             <SettingsIcon />
@@ -47,13 +47,16 @@ const Header = (props) => {
           <IconButton className={classes.button}>
             <HelpIcon />
           </IconButton>
-          <IconButton className={classes.button} onClick={e => toggleCancel()}>
+          <IconButton
+            className={classes.button}
+            onClick={(e) => toggleCancel()}
+          >
             <CancelIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
